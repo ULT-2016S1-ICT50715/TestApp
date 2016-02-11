bb6dd45a163062761e4d0ca653bd4ed97b9a0115
Nicholas Attlee <nicholas.attlee1@tafensw.edu.au>
2016-02-11 19:48:05 +1100

    Resolved merge conflicts with Areej's work

--- source/TestApp.HookIn/Program.cs
+++ source/TestApp.HookIn/Program.cs
@@ -11,6 +11,7 @@ namespace TestApp.HookIn
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world");
+            Console.WriteLine("HI my name is Abeer");
         }
     }
 }
